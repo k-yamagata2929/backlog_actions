ba5f058934297588ac2521986fd96250caea22ab
k-yamagata2929 <49549694+k-yamagata2929@users.noreply.github.com>
2020-05-12 17:45:54 +0900

    RETAIL-125 test #fix

--- index.js
+++ index.js
@@ -1,40 +1,93 @@
-console.log("Hello, World!");
-console.log(process.env.GITHUB_EVENT_PATH);
-
-const fs = require('fs');
-const jsonObject = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
-console.log(jsonObject.commits[0].message);
-
-const https = require('https');
-const issueID = 'RETAIL-125'
-const baseUrl = 'https://' + process.env.API_HOST + '/api/v2/issues/' + issueID + '/comments?apiKey=' + process.env.API_KEY
-console.log(baseUrl);
-console.log('----------');
-
+//import library
 const request = require('request');
-const postData = {
-    'content': jsonObject.commits[0].message
-};
-const postDataStr = JSON.stringify(postData);
+const fs = require('fs');
 
-const options = {
-  url: baseUrl,
-  method: 'POST',
-  json: postData
+//get commit message
+const jsonObject = JSON.parse(fs.readFileSync(process.env.GITHUB_EVENT_PATH, 'utf8'));
+console.log('---commit message---');
+console.log(jsonObject.commits[0].message);
+console.log('------');
+
+//set issueID
+let issueID = null;
+const jsonObjectSplit = jsonObject.split(' ', 3);
+if (jsonObjectSplit[0].match(process.env.RETAIL)) {
+    issueID = jsonObjectSplit[0];
 }
 
-console.log(options);
+//set commit message
+let commitMessage = null;
+commitMessage = jsonObjectSplit[1];
 
-request(
-  options,
-  (error, res, body) => {
-    if (error) {
-      console.error(error)
-      return
+//set statusId
+let statusID = null;
+const fixMessage = ['#fix', '#fixes', '#fixed'];
+const closeMessage = ['#close', '#closes', '#closed'];
+
+for (let i = 0; i < fixMessage.length; i++) {
+    if (jsonObjectSplit[2].match(fixMessage[i])) {
+      statusID = 3;
+      break;
     }
-    console.log(`statusCode: ${res.statusCode}`)
-    console.log(body)
-  }
-);
+}
 
+for (let j = 0; j < closeMessage.length; i++) {
+    if (jsonObjectSplit[2].match(closeMessage[j])) {
+      statusID = 4;
+      break;
+    }
+}
 
+// if issue id and commit message exist, add comment to backlog task
+if (issueID && commitMessage) {
+    const baseUrlComment = 'https://' + process.env.API_HOST + '/api/v2/issues/' + issueID + '/comments?apiKey=' + process.env.API_KEY;
+    
+    const postDataCommet = {
+      'content': commitMessage
+    };
+    
+    const optionsComment = {
+      url: baseUrlComment,
+      method: 'POST',
+      json: postDataCommet
+    };
+    
+    request(
+      optionsComment,
+      (error, res, body) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+      }
+    );
+}
+
+// if issue id and status id exist, update backlog task status
+if (issueID && statusID) {
+    const baseUrlStatus = 'https://' + process.env.API_HOST + '/api/v2/issues/' + issueID + '?apiKey=' + process.env.API_KEY;
+    
+    const postDataStatus = {
+      'statusId': statusID
+    };
+    
+    const optionsStatus = {
+      url: baseUrlStatus,
+      method: 'PATCH',
+      json: postDataStatus
+    };
+    
+    request(
+      optionsStatus,
+      (error, res, body) => {
+        if (error) {
+          console.error(error)
+          return
+        }
+        console.log(`statusCode: ${res.statusCode}`)
+        console.log(body)
+      }
+    );
+}
